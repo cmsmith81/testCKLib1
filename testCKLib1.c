28d9fc11ea5ee5e825a6569c23fee057d3e9e232
smith <smith@ieee.org>
2018-07-15 14:44:41 -0500

    library created

---
+++ testCKLib1.c
@@ -0,0 +1,17 @@
+//
+//  testCKLib1.c
+//  testCKLib1
+//
+//  Created by Corey Smith on 7/15/18.
+//  Copyright © 2018 srs. All rights reserved.
+//
+
+#include "testCKLib1.h"
+
+int sum(int a, int b) {
+    int sum;
+    
+    sum = a + b;
+    
+    return sum;
+}
---
+++ testCKLib1.h
@@ -0,0 +1,18 @@
+//
+//  testCKLib1.h
+//  testCKLib1
+//
+//  Created by Corey Smith on 7/15/18.
+//  Copyright © 2018 srs. All rights reserved.
+//
+
+#ifndef testCKLib1_h
+#define testCKLib1_h
+
+#include <stdio.h>
+
+int sum(int a, int b);
+
+#endif /* testCKLib1_h */
+
+
